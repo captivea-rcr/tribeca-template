61a2fdf703b13e565975d8f3b9d9b3db661cb82a
Ronald <ronald.crump@captivea.com>
2021-03-09 11:27:07 -0800

    Update Form For Customer Portal

--- add_fields_to_portal_form/models/name.py
+++ add_fields_to_portal_form/models/name.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.addons.portal.controllers import *
+from odoo.addons.portal.controllers.portal import *
 
 from odoo import api, fields, models, _
 
