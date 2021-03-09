b99caf7878b47f4088b9efa087c0214940b7b8c9
Ronald <ronald.crump@captivea.com>
2021-03-09 11:23:51 -0800

    Update Form For Customer Portal

---
+++ add_fields_to_portal_form/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Add Fields To Portal',
+    'version': '14.0.0.0.1',
+    'category': 'Portal',
+    'description': '''''',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
