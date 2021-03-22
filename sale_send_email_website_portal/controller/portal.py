1d7d19d0ebb930c35719d85492b16591ea500f13
Adarsh Patel <adarsh.patel@bistasolutions.com>
2021-03-22 18:25:33 +0530

    [IMP] improve code for display all the orders to the salesperson, and display orders to customers that state not in draft in portal view.

--- sale_send_email_website_portal/controller/portal.py
+++ sale_send_email_website_portal/controller/portal.py
@@ -1,13 +1,92 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import http
+from odoo import http, _
 from odoo.http import request
-from odoo.addons.http_routing.models.ir_http import slug
-from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
 
 
 class CustomerPortal(CustomerPortal):
+
+    def _prepare_home_portal_values(self, counters):
+        values = super()._prepare_home_portal_values(counters)
+        partner = request.env.user.partner_id
+
+        SaleOrder = request.env['sale.order']
+        if 'quotation_count' in counters:
+            if partner.customer_rank > 0:
+                values['quotation_count'] = SaleOrder.search_count([
+                    ('partner_id', '=', partner.id),
+                    ('state', 'in', ['sent', 'cancel']),
+                ]) if SaleOrder.check_access_rights('read', raise_exception=False) else 0
+            else:
+                values['quotation_count'] = SaleOrder.search_count([
+                    ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                    ('state', 'in', ['draft', 'sent', 'cancel']),
+                ]) if SaleOrder.check_access_rights('read', raise_exception=False) else 0
+        if 'order_count' in counters:
+            values['order_count'] = SaleOrder.search_count([
+                ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                ('state', 'in', ['sale', 'done'])
+            ]) if SaleOrder.check_access_rights('read', raise_exception=False) else 0
+
+        return values
+
+    @http.route(['/my/quotes', '/my/quotes/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        SaleOrder = request.env['sale.order']
+
+        if partner.customer_rank > 0:
+            domain = [
+                ('partner_id', '=', partner.id),
+                ('state', 'in', ['sent', 'cancel']),
+            ]
+        else:
+            domain = [
+                ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                ('state', 'in', ['draft', 'sent', 'cancel']),
+            ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Reference'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'state'},
+        }
+
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        quotation_count = SaleOrder.search_count(domain)
+        # make pager
+        pager = portal_pager(
+            url="/my/quotes",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=quotation_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # search the count to display, according to the pager data
+        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_quotations_history'] = quotations.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'quotations': quotations.sudo(),
+            'page_name': 'quote',
+            'pager': pager,
+            'default_url': '/my/quotes',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_quotations", values)
 
     @http.route(["/sale/send_email/<int:sale_id>"], type='http', auth="public", website=True)
     def sale_send_email(self, sale_id=None, **kw):
