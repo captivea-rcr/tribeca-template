22d8d8ec68219269888a3a749da8300643d6e244
Adarsh Patel <adarsh.patel@bistasolutions.com>
2021-03-25 15:12:25 +0530

    [IMP] Romove print statement.

--- survey_file_upload_field/controllers/main.py
+++ survey_file_upload_field/controllers/main.py
@@ -6,5 +6,4 @@ class DataSet(DataSet):
 
     @http.route(['/web/dataset/call_kw_survey', '/web/dataset/call_kw_survey/<path:path>'], type='json', auth="public")
     def call_kw_survey(self, model, method, args, kwargs, path=None):
-        print("\n\n4444444444----->", model, method, args, kwargs)
         return self._call_kw(model, method, args, kwargs)
